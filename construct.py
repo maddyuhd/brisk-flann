6ce1136f46a94cd7de377cc342d86969a95b8bd4
maddyuhd <krishnanmadhavan000@gmail.com>
2017-08-04 12:52:11 +0530

    server test 320 400

--- construct.py
+++ construct.py
@@ -1,78 +1,89 @@
 import tensorflow as tf
-from random import choice, shuffle, sample
-# from numpy import array
-import numpy as np
+from random import shuffle, sample
+# import numpy as np
 import progressbar
 import pickle
 from dump import brief_brisk as feat
 import os
+import sys
+import cv2
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tree, imagesInLeaves, nodes = {}, {}, {}
 nodeIndex = 0
-n_clusters = 16
-max_size_lev = 150
 
-# count = 0
+n_clusters = 8
+max_size_lev = 500
+
+inlocal = True
+# inlocal = False
+
+# detector = cv2.BRISK_create(50, 1, 1.0)
+
 
 class images:
-    def __init__(self, path,resVal=320 , id = None, link = None):
-        self.uuid = id
+
+    def __init__(self, path, resVal=320,
+                 detector=cv2.BRISK_create(50, 1, 1.0)):
         self.name = os.path.split(path)[-1][:-4]
-        self.s3link = link
-        self.path = path
-        self.kp, self.des = feat(path, resVal)
-        self.leafPos = []  #(index of leaf node, index position)
+        # self.s3link = link
+        self.kp, self.des = feat(path, resVal, detector)
+        self.leafPos = []  # (index of leaf node, index position)
 
     def updateLeaf(self, val):
         self.leafPos.append(val)
 
 
-def randomeCentroid(K,N):
+def randomeCentroid(K, N):
     return sample(xrange(0, K), N)
+
 
 def vecVal(a):
     x, y = a
     return x.des[y]
 
+
 class tfInit:
+
     def __init__(self, dim, n_clusters):
         self.n_clusters = n_clusters
         self.graph = tf.Graph()
-    
-        with tf.Session(graph = self.graph) as sess:
+
+        with tf.Session(graph=self.graph):
             with tf.device("/cpu:0"):
                 self.v1 = tf.placeholder("float", [dim])
                 self.v2 = tf.placeholder("float", [dim])
-                self.euclid_dist = tf.sqrt(tf.reduce_sum(tf.pow(tf.subtract
-                    (self.v1, self.v2), 2)))
+                self.euclid_dist = tf.sqrt(tf.reduce_sum(
+                    tf.pow(tf.subtract(self.v1, self.v2), 2)))
                 self.init_op = tf.global_variables_initializer()
 
-
-    def setRandomCentroid(self,vectors):
+    def setRandomCentroid(self, vectors):
         vector_indices = randomeCentroid(len(vectors), self.n_clusters)
 
-        with tf.Session(graph = self.graph) as sess:
+        with tf.Session(graph=self.graph):
             self.centroidPh = tf.placeholder("uint8", [self.n_clusters])
             self.cluster_assignment = tf.argmin(self.centroidPh, 0)
-            self.centroidsVal = [tf.Variable((vecVal(vectors[vector_indices[i]])))
-                            for i in range(self.n_clusters)]
+            self.centroidsVal = [tf.Variable((vecVal(
+                vectors[vector_indices[i]]))) for i in range(self.n_clusters)]
 
     def updateRandomCentroid(self, vectors):
         self.vector_indices = randomeCentroid(len(vectors), self.n_clusters)
-        with tf.Session(graph = self.graph) as sess:
-            self.centroidsVal = [self.centroidsVal[i].assign((vecVal(vectors[self.vector_indices[i]])))
-                            for i in range(self.n_clusters)]
+
+        # with tf.Session(graph=self.graph) as sess:
+        self.centroidsVal = [self.centroidsVal[i].assign((vecVal(
+            vectors[self.vector_indices[i]])))
+            for i in range(self.n_clusters)]
 
     def finalVariable(self):
-        with tf.Session(graph = self.graph) as sess:
+        with tf.Session(graph=self.graph) as sess:
             sess.run(self.init_op)
 
 
 class progress:
     def __init__(self, msg, max):
         self.count = 0
-        self.widgets = [str(msg)+":", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
+        self.widgets = [str(msg) + ":", progressbar.Percentage(),
+                        " ", progressbar.Bar(), " ", progressbar.ETA()]
         self.pbar = progressbar.ProgressBar(maxval=max, widgets=self.widgets)
         self.pbar.start()
 
@@ -84,38 +95,38 @@ class progress:
         self.pbar.finish()
 
 
-def saveFile(object, name, debug = False):
+def saveFile(object, name, debug=False):
     if debug:
         f = open("module/" + name + '.pckl', 'wb')
     else:
         f = open("/home/ubuntu/brisk-flann/module/" + name + '.pckl', 'wb')
     pickle.dump(object, f)
-    print "[INFO] saved " + name
+    # print "[INFO] saved " + name
     f.close()
 
-def openFile(object, debug = False):
+
+def openFile(object, debug=False):
     if debug:
-        f = open("module/" + object+'.pckl', 'rb')
+        f = open("module/" + object + '.pckl', 'rb')
     else:
-        f = open("/home/ubuntu/brisk-flann/module/" + object+'.pckl', 'rb')
+        f = open("/home/ubuntu/brisk-flann/module/" + object + '.pckl', 'rb')
     return pickle.load(f)
+
 
 def constructTree(node, vectors, bar):
     global nodeIndex, nodes, tree, imagesInLeaves
     tree[node] = []
 
     if (len(vectors) < max_size_lev):
-        
         imagesInLeaves[node] = []
-        
+
         for idx, v in enumerate(vectors):
-            
+
             imagesInLeaves[node].append(v)
             v[0].updateLeaf((node, idx))
 
-            #fansy output
+            # fansy output
             bar.update()
-        
         # print "leaves_node ", node
 
     else:
@@ -124,13 +135,14 @@ def constructTree(node, vectors, bar):
         shuffle(vector_indices)
 
         graph = tf.Graph()
-        with tf.Session(graph = graph) as sess:
+        with tf.Session(graph=graph) as sess:
         # with graph.as_default():
             # sess = tf.Session()
             with tf.device("/cpu:0"):
                 childIDs = [[] for i in range(n_clusters)]
-                centroidsVal = [tf.Variable((vecVal(vectors[vector_indices[i]])))
-                                for i in range(n_clusters)]
+                centroidsVal = [tf.Variable((
+                    vecVal(vectors[vector_indices[i]])))
+                    for i in range(n_clusters)]
 
                 centroidPh = tf.placeholder("uint8", [dim])
 
@@ -138,8 +150,8 @@ def constructTree(node, vectors, bar):
                 v2 = tf.placeholder("float", [dim])
 
                 # Euclidean distances
-                euclid_dist = tf.sqrt(tf.reduce_sum(tf.pow(tf.subtract(v1, v2), 2)))
-
+                euclid_dist = tf.sqrt(tf.reduce_sum(
+                    tf.pow(tf.subtract(v1, v2), 2)))
                 centroidPh = tf.placeholder("float", [n_clusters])
                 cluster_assignment = tf.argmin(centroidPh, 0)
 
@@ -163,14 +175,19 @@ def constructTree(node, vectors, bar):
 
                 for i in range(n_clusters):
                     nodeIndex = nodeIndex + 1
-                    nodes[nodeIndex] = vectors[vector_indices[i]]  ##need changes
+                    # need_changes !
+                    #              v
+                    nodes[nodeIndex] = vectors[vector_indices[i]]
                     tree[node].append(nodeIndex)
                     constructTree(nodeIndex, childIDs[i], bar)
 
 
-
 if __name__ == "__main__":
-    
+
+    if (inlocal):
+        n_clusters = int(sys.argv[1])
+        max_size_lev = int(sys.argv[2])
+
     features = []
     rootDir = 'data/1'
     fileList = sorted(os.listdir(rootDir))
@@ -180,7 +197,7 @@ if __name__ == "__main__":
         img = images(img_path)
         # kp, des = feat(img_path)
         for i in range(len(img.des)):
-            features.append((img,i))
+            features.append((img, i))
         # del kp, des
 
     bar = progress("Constructing", len(features))
@@ -192,8 +209,9 @@ if __name__ == "__main__":
     constructTree(0, features, bar)
 
     bar.finish()
-
-    saveFile(tree,"tree")
-    saveFile(imagesInLeaves,"imagesInLeaves")
-    saveFile(nodes,"nodes")
-    print("[INFO] indexed {} images, {} vectors".format(len(fileList), len(features)))
+    inlocal = True
+    saveFile(tree, "tree", inlocal)
+    saveFile(imagesInLeaves, "imagesInLeaves", inlocal)
+    saveFile(nodes, "nodes", inlocal)
+    # print("[INFO] indexed {} images, {} vectors".format(
+    #     len(fileList), len(features)))
--- traverse.py
+++ traverse.py
@@ -1,23 +1,26 @@
+from time import time
 import numpy as np
 import collections
 import os
-import sys
-import progressbar
+# import progressbar
 import tensorflow as tf
 from heapq import heappush, heappop
-from construct import vecVal, openFile, images, tfInit
+from construct import vecVal, openFile, images, tfInit, progress, inlocal
 
+start = time()
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 Lmax = 50
 
+
 def hamming2(s1, s2):
-    assert len(s1) == len(s2)
+    # assert len(s1) == len(s2)
+
     # print s1,s2
-    r = (1 << np.arange(8))[:,None]
+    r = (1 << np.arange(8))[:, None]
     # print r
     # cv2.norm(s1, s2[, normType[, mask]])
-    # return sum(c1 != c2 for c1, c2 in zip(s1, s2))
-    return np.count_nonzero((np.bitwise_xor(s1,s2) & r) != 0)
+    # return np.count_nonzero((s1 & r) != (s2 & r))
+    return np.count_nonzero((np.bitwise_xor(s1, s2) & r) != 0)
 
 
 def traverseTree(obj, N, PQ, Q):
@@ -32,7 +35,7 @@ def traverseTree(obj, N, PQ, Q):
             length += 1
 
     except:
-        
+
         C = tree[N]
 
         if len(C) > 0:
@@ -42,12 +45,10 @@ def traverseTree(obj, N, PQ, Q):
             #     sess = tf.Session()
             with tf.Session(graph=obj.graph) as sess:
             #     with tf.device("/cpu:0"):
-
             #         v1 = tf.placeholder("float", [dim])
             #         v2 = tf.placeholder("float", [dim])
             #         euclid_dist = tf.sqrt(tf.reduce_sum(tf.pow(tf.subtract(v1, v2), 2)))
             #         # centroid = tf.Variable((Q))
-     
             #         init_op = tf.global_variables_initializer()
             #         sess.run(init_op)
 
@@ -62,7 +63,7 @@ def traverseTree(obj, N, PQ, Q):
             Cq = heappop(data)[1]
 
             for i in data:
-                heappush(PQ,i)
+                heappush(PQ, i)
 
             traverseTree(obj, Cq, PQ, Q)
 
@@ -70,7 +71,7 @@ def traverseTree(obj, N, PQ, Q):
 def searchTree(obj, T, Q, i):
     global Lmax, length, result
 
-    length = 0 
+    length = 0
     PQ, result = [], []
 
     # for ti in T:
@@ -82,54 +83,89 @@ def searchTree(obj, T, Q, i):
         # print "inside",length
         try:
             N = heappop(PQ)[1]
-        except:
-            raise
+        except Exception as e:
+            raise e
 
         traverseTree(obj, N, PQ, Q)
-    pbar.update(i)
+    # pbar.update(i)
+
+
+def accuracy(name, y):
+    global count
+    for i in range(len(y)):
+        if name == y[i][0]:
+            count += 1
+            break
 
 
 if __name__ == "__main__":
-    # inlocal = True
-    inlocal = False
+
+    if (inlocal):
+        global count
+        count = 0
 
     tree = openFile("tree", inlocal)
     imagesInLeaves = openFile("imagesInLeaves", inlocal)
     nodes = openFile("nodes", inlocal)
 
+    import sys
+    import cv2
+
     if (inlocal):
-        imgname = str(sys.argv[1])
-        img_path = "data/2/00"+imgname+".jpg"
+        import glob
+        img_path = glob.glob("data/2/*.jpg")
+        # print img_path
+        # imgname = str(sys.argv[1])
+        # img_path = "data/2/00"+imgname+".jpg"
     else:
         img_path = str(sys.argv[1])
 
-    img = images(img_path, 320)
+    bar = progress("Traversing", len(img_path))
+    star = cv2.xfeatures2d.StarDetector_create(15, 30, 10, 8, 5)
 
-    hello=[]
-    tfObj = tfInit(32, 16)
-    tfObj.finalVariable()
+    for imgp in img_path:
+        img = images(imgp, 400, star)
+
+        hello = []
+        if (inlocal):
+            n_clusters = int(sys.argv[1])
+        else:
+            from construct import n_clusters
+
+        tfObj = tfInit(32, n_clusters)
+        tfObj.finalVariable()
+
+    # if (inlocal):
+        # print "[INFO] total items : ", len(img.des)
+        # widgets = ["Traversing: ", progressbar.Percentage(),
+        #             " ", progressbar.Bar(), " ", progressbar.ETA()]
+    #     pbar = progressbar.ProgressBar(maxval=len(img.des), widgets=widgets)
+    #     pbar.start()
+
+        for i, d in enumerate(img.des):
+
+            searchTree(tfObj, 0, d, i)
+
+            for _ in range(5):
+                help = heappop(result)
+                hello.append(help[1][0].name)
+
+        # pbar.finish()
+
+        y = collections.Counter(hello).most_common(5)
+        if (inlocal):
+            # print "{} : {}".format(img.name,y)
+            accuracy(img.name, y)
+            bar.update()
+        else:
+            import json
+            d = {'id0': y[0][0], 'id1': y[1][0],
+                 'id2': y[2][0], 'id3': y[3][0],
+                 't': str(time() - start) + "sec"}
+            print json.dumps(d)
+    bar.finish()
 
     if (inlocal):
-        print "[INFO] total items : ", len(img.des)
-    widgets = ["Traversing: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
-    pbar = progressbar.ProgressBar(maxval=len(img.des), widgets=widgets)
-    pbar.start()
-
-    for i,d in enumerate(img.des):
-
-        searchTree(tfObj, 0, d, i)
-
-        for _ in range(5):
-            help = heappop(result)
-            hello.append(help[1][0].name)
-    
-    pbar.finish()
-
-    y = collections.Counter(hello).most_common(5) 
-    if (inlocal):
-        print "{} : {}".format(img.name,y)
-    else:
-        import json
-        d={'id0':y[0][0],'id1':y[1][0], 'id2':y[2][0], 'id3':y[3][0]}
-        print json.dumps(d)
-        # return y[0][0]
+        a = count / float(len(img_path)) * 100
+        print a
+        # print "[INFO] final accuracy {}%".format(a * 100)
