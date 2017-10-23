3cc9142c11bebc307ae638e91fe0813cecda5254
Madhavan Krishnan <krishnanmadhavan000@gmail.com>
2017-10-23 18:00:05 +0530

    path for log

--- features/info.py
+++ features/info.py
@@ -3,8 +3,8 @@ inlocal = True  # False
 n_clusters = 8
 max_size_lev = 500
 
-# logPath = "/var/www/html/snappar.log"
-logPath = "/home/smacar/Desktop/work/brisk-flann/log/snappar_studio.log"
+logPath = "/var/www/html/system/storage/logs/engine.log"
+# logPath = "/home/smacar/Desktop/work/brisk-flann/log/snappar_studio.log"
 
 success = "0 "
 failed = "1 "
